d2e71382bb0afedd3848b353794cd3f3ceee433c
naporitanOmega <su5412ar14@gmail.com>
2019-11-18 15:40:15 +0900

    modified result.php

--- result.php
+++ result.php
@@ -1,8 +1,13 @@
 <?php
 require_once('common.php');
 
-$qid = $_GET['qid'];
+$qid = isset($_GET['qid'])? $_GET['qid']:-1;
 $answer = $_GET['answer'];
+
+if($qid == -1 || is_numeric($qid) || (0 <= $qid) && ($qid<count($question))){
+    echo 'error: $qid invalid';
+    exit(1);
+}
 
 if($question[$qid][1] == $answer){
     echo "seikai";
